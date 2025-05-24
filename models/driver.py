d1c178a0237e608b94fe6f1a0f9d80ce0f9989c1
danial.moafi <danial.moafi@snapp.cab>
2025-05-24 17:51:06 +0200

    add features

---
+++ models/driver.py
@@ -0,0 +1,7 @@
+        
+class Driver:
+    def __init__(self, start_time, end_time, start_location, end_location):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.start_location = start_location
+        self.end_location = end_location
---
+++ models/map.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import h3
+
+class Map:
+    def __init__(self):
+        try:
+            self.data = pd.read_csv('data/databases/hexagons.csv')
+            self.districts = self.data['district'].tolist()
+            self.distance_matrix = np.load('data/databases/distance_matrix.npy')
+            self.hexagon_weights = self.data['weight'].tolist()
+            self.neighbors = self._find_neighbors()
+        except Exception as e:
+            print(f"Error loading map data, Please run data/map_data.py to generate the data")
+            raise e
+        
+    def _find_neighbors(self):
+        neighbors = {}
+        for i in range(len(self.districts)):
+            neighbors[self.districts[i]] = list(h3.grid_ring(self.districts[i], 2))
+        return neighbors
+    
+    def get_distance(self, origin, destination):
+        return self.distance_matrix[self.districts.index(origin), self.districts.index(destination)]
+    
+    def get_hexagon_weight(self, hexagon):
+        return self.hexagon_weights[hexagon]
+    
+    def get_cost(self, origin, destination):
+         # 1.3 is approxiation ratio for air distance to road distance, 0.7 is a factor for the cost of the ride
+        return self.distance_matrix[self.districts.index(origin), self.districts.index(destination)] * 1.3 * 1.1
+    
+    def get_neighbors(self, hexagon):
+        return self.neighbors[hexagon]
+        
+    def get_time(self, origin, destination):
+        return self.distance_matrix[self.districts.index(origin), self.districts.index(destination)] * 1.3
